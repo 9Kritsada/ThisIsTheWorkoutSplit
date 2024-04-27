cd9b73e34a12ec8df828718e3c8adc34f7440eac
9Kritsada <kritsada.wiriya@gmail.com>
2024-04-27 20:45:53 +0700

    fix bg-fullscreen on lightMdoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./css/loader.css";
+import { dark } from "@mui/material/styles/createPalette";
 
 const App = () => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -166,7 +167,7 @@ const App = () => {
         <main className="bg-zinc-600 dark:bg-black">
           <img
             src="/112_ORANGE.jpg"
-            className={`${!isLoaded ? 'hidden' : 'opacity-30 fixed h-full w-full  object-center object-cover inset-0'}`}
+            className={`${!isLoaded || darkMode ? 'hidden' : 'opacity-30 fixed h-full w-full  object-center object-cover inset-0'}`}
             onLoad={handleImageLoad}
           />
           <h1 className="text-4xl sm:text-5xl md:text-6xl font-black text-center head-font dark:text-[#ff0000] z-10">
